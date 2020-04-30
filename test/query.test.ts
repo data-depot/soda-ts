0f3623d25dfe7ffd23eb177b4b738d6fc3837f6e
Walee Ahmed <dev@walee.me>
2020-04-30 03:35:19 -0400

    test: 100% coverage for query file

--- test/query.test.ts
+++ test/query.test.ts
@@ -14,4 +14,33 @@ describe('query', () => {
       clauses: []
     })
   })
+
+  it('custom domain', () => {
+    const DOMAIN = 'test.test.nyc'
+    const query = createQuery(SRC, {
+      domain: DOMAIN
+    })
+
+    expect(query.domain).toBe(DOMAIN)
+  })
+
+  it('default domain', () => {
+    const query = createQuery(SRC)
+
+    expect(query.domain).toBe('data.cityofnewyork.us')
+  })
+
+  it('default api path', () => {
+    const query = createQuery(SRC)
+
+    expect(query.apiPath).toBe('resource')
+  })
+
+  it('set api path', () => {
+    const query = createQuery(SRC, {
+      apiPath: 'api/catalog/v1'
+    })
+
+    expect(query.apiPath).toBe('api/catalog/v1')
+  })
 })
