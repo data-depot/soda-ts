bc0ecc981756d1b4a3d420f77cc5d917288c520b
Walee Ahmed <dev@walee.me>
2020-04-30 03:49:50 -0400

    test: added coverage rule

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['./config/testSetup.ts']
+  setupFilesAfterEnv: ['./config/testSetup.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 90,
+      lines: 90,
+      statements: 90
+    }
+  }
 }
