4a4e61f6157d2775ecb62d1be3a7439a7493a5d9
Walee Ahmed <dev@walee.me>
2020-05-07 06:07:08 -0400

    fix: release config

--- release.config.js
+++ release.config.js
@@ -1,27 +1,26 @@
 module.exports = {
-  dryRun: false,
-  branches: ["master"],
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
+  "dryRun": false,
+  "plugins": [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
     [
-      '@semantic-release/changelog',
+      "@semantic-release/changelog",
       {
-        changelogFile: 'CHANGELOG.md'
+        "changelogFile": "docs/CHANGELOG.md"
       }
     ],
-    '@semantic-release/npm',
-    '@semantic-release/github',
+    "@semantic-release/npm",
+    "@semantic-release/github",
     [
-      '@semantic-release/git',
+      "@semantic-release/git",
       {
-        assets: [
-          'CHANGELOG.md',
-          'package.json',
-          'package-lock.json'
+        "assets": [
+          "docs/CHANGELOG.md",
+          "package.json",
+          "package-lock.json"
         ],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
       }
     ]
   ]
-}
+};
